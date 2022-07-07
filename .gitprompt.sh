22626e3d65a866b0fd9fae8c4f05a5d84c168280
Brian Hou <brian.hou@live.com>
2022-07-07 12:43:59 -0700

    [bash] Clean up git colorizing prompt.

--- .gitprompt.sh
+++ .gitprompt.sh
@@ -1,29 +1,24 @@
-# TODO: Figure out how to use the built-in git-prompt.sh
-
 # Adding git functions from http://amatsukawa.com/git-branch-command-line.html
+
 parse_git_branch () {
-    if git rev-parse --git-dir >/dev/null 2>&1
-    then
-	echo -e \($(git branch 2>/dev/null | sed -n '/^\*/s/^\* //p')\)
+    if git rev-parse --git-dir >/dev/null 2>&1; then
+        echo -e \($(git branch 2>/dev/null | sed -n '/^\*/s/^\* //p')\)
     fi
 }
 
-function git_color {
-    local STATUS=`git status 2>&1`
-    if [[ "$STATUS" == *'Not a git repository'* ]]
-    then
-	echo ""
+git_color () {
+    local status=`git status 2>&1`
+    if [[ "$status" == *'Not a git repository'* ]]; then
+        echo ""
     else
-	if [[ "$STATUS" != *'working'*'clean' ]]
-	then
-	    echo -e '\e[0;31m'		# red if need to commit
-	else
-	    if [[ "$STATUS" == *'Your branch is ahead'* ]]
-	    then
-		echo -e '\e[0;33m'	# yellow if need to push
-	    else
-		echo -e '\e[0;36m'	# else cyan
-	    fi
-	fi
+        if [[ "$status" != *'working'*'clean' ]]; then
+            echo -e '\e[0;31m'      # red if need to commit
+        else
+            if [[ "$status" == *'Your branch is ahead'* ]]; then
+                echo -e '\e[0;33m'  # yellow if need to push
+            else
+                echo -e '\e[0;36m'  # else cyan
+            fi
+        fi
     fi
 }
